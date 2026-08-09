708c62f89133eba11dc13a1dd13bce063f517117
firefoxic <firefoxic.dev@gmail.com>
2026-08-09 13:54:12 +0300

    Move the CI reporter setting from the action to the `vitest` config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
+import { env } from "node:process"
+
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
 	test: {
 		setupFiles: [`./vitest.setup.ts`],
-		reporters: [`dot`],
+		reporters: env.CI ? [`github-actions`, `default`] : [`dot`],
 		isolate: false,
 		watch: false,
 		coverage: {
